aa9bee3ded6d171428746217c235f766f10c084c
samkitjain1802 <jainsamkit816@gmail.com>
2022-08-17 22:11:44 +0530

    commit

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -77,34 +77,34 @@ function SetAvatar() {
   return (
     <>
         {
-            isLoading ? 
+          isLoading ? 
+          <Container>
+              <img src={loader} alt="loader" className="loader" />
+          </Container> : 
+          (
             <Container>
-                <img src={loader} alt="loader" className="loader" />
-            </Container> : 
-            (
-                <Container>
-                    <div className="title-container">
-                        <h1>Pick an avatar as your profile picture</h1>
-                    </div>
-                    <div className="avatars">
-                        {avatars.map((avatar, index) => {
-                            return(
-                                <div 
-                                    key={index} 
-                                    className={`avatar ${selectedAvatar === index ? "selected" : ""}`}
-                                >
-                                    <img 
-                                        src={`data:image/svg+xml;base64,${avatar}`} 
-                                        alt="avatar" 
-                                        onClick={()=>setSelectedAvatar(index)} 
-                                    />                            
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <button className="submit-btn" onClick={setProfilePicture}>Set as Profile Picture</button>
-                </Container>
-            )
+              <div className="title-container">
+                  <h1>Pick an avatar as your profile picture</h1>
+              </div>
+              <div className="avatars">
+                {avatars.map((avatar, index) => {
+                  return(
+                      <div 
+                          key={index} 
+                          className={`avatar ${selectedAvatar === index ? "selected" : ""}`}
+                      >
+                          <img 
+                              src={`data:image/svg+xml;base64,${avatar}`} 
+                              alt="avatar" 
+                              onClick={()=>setSelectedAvatar(index)} 
+                          />                            
+                      </div>
+                  );
+                })}
+              </div>
+              <button className="submit-btn" onClick={setProfilePicture}>Set as Profile Picture</button>
+            </Container>
+          )
         }
         <ToastContainer />
     </>
